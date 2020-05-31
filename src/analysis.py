d77b8724735dac888bdb170a6ef744a90eeb3a78
edgaregurrola <edgaregurrola@hotmail.com>
2020-05-31 00:26:59 -0500

    Manejo de datos de calidad del aire AQIP y CdMX

---
+++ src/analysis.py
@@ -0,0 +1,12 @@
+################################################################################
+# Module: Analysis data
+# developed by: Edgar Egurrola
+# 			  edgar.egurrola@tec.mx
+# updated: 28/05/2020
+################################################################################
+
+import os
+import pandas as pd
+import numpy as np
+
+dir_grl = ''
